e3f80bbcfe8ead2dab620341b409edbc61b1e84e
Maximo Bautista <max.bautista97@gmail.com>
2019-01-30 14:09:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,13 @@ class Person
   def name=(assigned_name)
     @name = assigned_name
   end
+    def name 
+      @name
+    end
   def job=(job)
     @job = job
+  end
+  def job
+    @job
   end
 end

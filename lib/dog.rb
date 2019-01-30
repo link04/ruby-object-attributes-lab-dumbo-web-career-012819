6586b75b0c26b16a4e0a204d12762cff0946864a
Maximo Bautista <max.bautista97@gmail.com>
2019-01-30 14:09:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+    def name=(assigned_name)
+      @name = assigned_name
+    end
+    def name 
+      @name
+    end
+    
+    def breed=(dog_dreed)
+      @breed = dog_dreed
+    end
+    def breed 
+      @breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(assigned_name)
+    @name = assigned_name
+  end
+    def name 
+      @name
+    end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+end
